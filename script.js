19ed7788d4ebd569a8062f256c5741fd7c01c4ad
reswanth-veerendra <reswanthveerendrakarri@gmail.com>
2026-04-02 14:20:06 +0530

    ....

---
+++ script.js
@@ -0,0 +1,63 @@
+async function getUser(){
+    const username = document.getElementById('username').value;
+    const profile = document.getElementById('profile');
+    const reposit = document.getElementById('reposit');
+    
+    if (!username) return alert("Please enter a name");
+
+profile.innerHTML = "<p>Loading profile...</p>";
+reposit.innerHTML = "<p>Loading repositories...</p>";
+    try {
+        const uresponse = await fetch(`https://api.github.com/users/${username}`,
+            {headers:{
+                     Authorization:"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"
+                }
+            }
+        );
+        const user = await uresponse.json();
+
+        if (user.message === "Not Found") {
+            profile.innerHTML = "<p>User not found!</p>";
+            return;
+        }
+        const rresponse = await fetch(`https://api.github.com/users/${username}/repos?sort=created&per_page=50`,
+            {headers:{
+                     Authorization:"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"
+                }
+            });
+        const repos = await rresponse.json();
+        displayUser(user, repos);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+function displayUser(user, repos) {
+    const profile = document.getElementById('profile');
+    const reposit = document.getElementById('reposit');
+       
+    const repolinks = repos.map(repo=>`
+        <div class="repo-card">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            <p>${repo.description||"No Description Available"}</p>
+            <span>⭐ ${repo.stargazers_count}</span>
+        </div>
+    `).join('');
+
+profile.innerHTML = `
+    <div class="profilecard">
+        <img src="${user.avatar_url}" width="100">
+        <h2>${user.name || user.login}</h2>
+        <hr>
+        <p><b>Bio:</b><br>${user.bio || 'No bio available'}</p>
+        <p><b>Company:</b> ${user.company || 'Not in any company'}</p>
+        <p><b>Location:</b> ${user.location || 'Not specified'}</p>
+        <p><b>Followers:</b> ${user.followers} | <b>Following:</b> ${user.following}</p>
+        <p><b>Public Repos:</b> ${user.public_repos}</p>
+    </div>
+`;
+    reposit.innerHTML = `
+           <h3>Latest Repositories:</h3>
+                  ${repolinks}
+    `;
+}
